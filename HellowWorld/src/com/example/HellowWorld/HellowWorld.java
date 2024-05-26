6e778b8c71a4b472e63d1f3ad3fe71fabaf99be8
Gaowtam Barua <128220812+Gaowtam@users.noreply.github.com>
2024-05-26 11:06:21 +0600

    Add files via upload

---
+++ HellowWorld/src/com/example/HellowWorld/HellowWorld.java
@@ -0,0 +1,12 @@
+package com.example.HellowWorld;
+
+public class HellowWorld {
+	public static void main(String args[])
+	{
+		int i=1;
+		for(i=0;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
