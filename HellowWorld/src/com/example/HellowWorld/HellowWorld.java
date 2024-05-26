22782603a3737397ff776ec466478c553b8f8d62
Gaowtam Barua <128220812+Gaowtam@users.noreply.github.com>
2024-05-26 11:21:04 +0600

    update

--- HellowWorld/src/com/example/HellowWorld/HellowWorld.java
+++ HellowWorld/src/com/example/HellowWorld/HellowWorld.java
@@ -3,10 +3,6 @@ package com.example.HellowWorld;
 public class HellowWorld {
 	public static void main(String args[])
 	{
-		int i=1;
-		for(i=0;i<=100;i++)
-		{
-			System.out.println(i);
-		}
+		System.out.println("Hellow World");
 	}
 }
